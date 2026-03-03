c4bca64a134c77b4de16ffa4f4ee95a83a8293d0
Ruggery <ruggeryalves@hotmail.com>
2026-03-03 13:37:36 +0000

    Add basic CLI parsing with version & search
    Learning how to use some logic of programming, methods, search and version inputs. Class - 03/03/2026

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,5 +1,39 @@
-import 'package:cli/cli.dart' as cli;
+import 'dart:io';
+
+const version = '0.0.1';
 
 void main(List<String> arguments) {
-  print('Hello world: ${cli.calculate()}!');
+  if (arguments.isEmpty) {
+    print('Hello world: DART!');
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  } else {
+    printUsage();
+  }
+}
+
+// Catch-all for any unrecognized command.
+void printUsage() {
+  print(
+    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'",
+  );
+}
+
+void searchWikipedia(List<String>? arguments) {
+  // ? after List<String> means that the arguments can be null.
+  final String articleTitle;
+
+  // If the user didn't pass in arguments, request an article title.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    // Await input and provide a default empty string if the input is null.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Otherwise, join the arguments into a single string.
+    articleTitle = arguments.join(' ');
+  }
+  print('Current article title: $articleTitle');
 }
