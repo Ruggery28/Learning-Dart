50bb2d18b1e08f7a4c8f5d68f85c75059e348f74
Ruggery <ruggeryalves@hotmail.com>
2026-03-10 13:39:15 +0000

    Add Wikipedia search command and http dep
    Add a search subcommand to the CLI that looks up Wikipedia article summaries. Imports the http package, makes searchWikipedia async, reads title from args or stdin (with empty input handling), and calls getWikipediaArticle to fetch and print the article summary. Also add the http dependency to pubspec.yaml and adjust usage/help behavior.

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hello world: DART!');
-  } else if (arguments.first == 'version') {
-    print('Dartpedia CLI version $version');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
   } else if (arguments.first == 'search') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
     searchWikipedia(inputArgs);
@@ -22,7 +21,7 @@ void printUsage() {
   );
 }
 
-void searchWikipedia(List<String>? arguments) {
+void searchWikipedia(List<String>? arguments) async{
   // ? after List<String> means that the arguments can be null.
   final String articleTitle;
 
@@ -30,10 +29,31 @@ void searchWikipedia(List<String>? arguments) {
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
     // Await input and provide a default empty string if the input is null.
-    articleTitle = stdin.readLineSync() ?? '';
+    final inputFromStdin = stdin.readLineSync();
+    if(inputFromStdin == null || inputFromStdin.isEmpty){
+      print("No article title provided. Exiting");
+      return;
+    }
+    articleTitle = inputFromStdin;
   } else {
     // Otherwise, join the arguments into a single string.
     articleTitle = arguments.join(' ');
   }
-  print('Current article title: $articleTitle');
+  print("Looking up articles about '$articleTitle' .Please wait");
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
+}
+
+//Method to search a article on wikipedia
+Future<String> getWikipediaArticle(String articleTitle) async{
+  final url = Uri.https(
+    "en.wekipedia.org", //http link
+    "/api/rest_v1/page/summary/$articleTitle" ,//API path
+  );
+  final response = await http.get(url); //this send the hhtp request and waits for the server to respond. The response is stored in the variable response.
+  if(response.statusCode == 200){ //200 means that the request was successful and the server returned the requested data. If the status code is 200, we can access the content of the article through response.body, which contains the body of the HTTP response as a string.
+    return response.body;
+  } 
+
+  return "Error: Failed to fecth article: $articleTitle .Status code: ${response.statusCode}";
 }
